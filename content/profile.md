035c5fd4bf995c2b8bc5a489aa81d7585e29896f
Manasvi Gupta <manasvigupta@yahoo.co.in>
2013-05-28 00:20:52 +0530

    Initial Post's

---
+++ content/profile.md
@@ -0,0 +1,10 @@
+Title: About me
+Date: 2013-05-27 08:00
+Category: Technical
+Author: Manasvi Gupta
+Slug: profile
+Summary: Summary about me
+
+Here are some details about me..
+
+THis is my profile
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,39 +1,71 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
 
-THEME='flasky'
+import os
+import sys
+sys.path.append(os.getcwd())
 
-AUTHOR = u'Manasvi Gupta'
-SITENAME = u'Manasvi Gupta'
+ROOT = os.path.realpath(os.getcwd())
+#THEME = os.path.join(ROOT,'theme')
+
+THEME = 'theme'
+AUTHOR = u"Manasvi Gupta"
+SITETITLE= u"Manasvi Gupta's blog"
+SITENAME = u"Will code for FUN!"
+SITESUBTITLE = u"Open Source, Startups, Machine Learning, Functional Programming"
 SITEURL = ''
 
-TIMEZONE = 'Europe/Paris'
 
-DEFAULT_LANG = u'en'
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = False
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = False
+#PAGE_DIR='src/pages'
+TIMEZONE = 'Europe/London'
+GITHUB_REPO = "https://github.com/manasvigupta/website"
+GIT_BRANCH = 'master'
+GITHUB_EDIT_PREFIX = "%s/blob/%s" % (GITHUB_REPO, GIT_BRANCH)
+TWITTER_USERNAME = 'manasvigupta'
+TWITTER_SHARE_RELATED = TWITTER_USERNAME
+TWITTER_SHARE_VIA = TWITTER_USERNAME
 
 
-# Add CNAME file 
-FILES_TO_COPY = (
-    ('CNAME', 'CNAME'),
-)
+DEFAULT_LANG='en'
+GOOGLE_ANALYTICS = "UA-34383847-1"
+# Social widgets
+# Name , link, new page
+SOCIAL = (
+
+     ('Me', '/profile.html' , False ),
+     ('Twitter', 'https://twitter.com/manasvigupta', True),
+    ('Linkedin', 'http://www.linkedin.com/manasvigupta', True),
+    ('Github', 'http://github.com/manasvigupta/', True),
+         )
+
+# Links
+LINKS = (
+
+         )
+
+DEFAULT_PAGINATION = 7
+DEFAULT_ORPHANS = 1
+
+DISQUS_SITENAME = "rach-belaid-blog"
+
+DEFAULT_DATE_FORMAT = "%d %b %Y"
+# A list of files to copy from the source to the destination
+#FILES_TO_COPY = (('extra/robots.txt', 'robots.txt'),('extra/CNAME', 'CNAME'),)
+
+#DIRECT_TEMPLATES= ['index', 'tags', 'categories', 'archives','profile'] #,'resume','projects']
+#EXTRA_TEMPLATES_PATHS=['extra',]
+PROFILE_SAVE_AS = 'profile.html'
+RESUME_SAVE_AS = 'resume.html'
+PROJECTS_SAVE_AS = 'projects.html'
+#PLUGIN_PATH = "plugins"
+#from plugins.bitly import bitly_plugin
+#PLUGINS = ['pelican-bitly',]
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
+    
+    
+    
